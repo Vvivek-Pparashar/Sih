3b600e16ed6a0a533f52126d9307bba7f7e282b3
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2023-09-16 00:25:01 +0530

    yeh! we started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="l-m">
+        <div className="l-m-f">
+          <nav>Vivek isgr8</nav>
+
+          <div className="l-m-f-c">
+            <div>
+              <h1>
+                Perfect place for <br />
+                your unique <br />
+                idea✌
+              </h1>
+              <p>
+                Businesses generally promote their brand, products, and services
+                by identifying audience
+              </p>
+
+              <img src={require("./comment.png")} alt="heroImg" className="comment-img" />
+            </div>
+            <img src={require("./heroImg.png")} alt="heroImg" className="hero-img" />
+          </div>
+        </div>
+        <div className="l-m-s">sdkfjslkdfj</div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
