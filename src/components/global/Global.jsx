e046c1e5ea3e1ee449253dd764c20eaec5e5c22c
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2023-10-08 20:54:47 +0530

    responsive

--- src/components/global/Global.jsx
+++ src/components/global/Global.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./Global.css";
 
 const Global = () => {
   return (
-    <div style={{
-        width:"100%",
-        display:"flex",
-        flexDirection:"column",
-        alignItems:"center",
-        textAlign:"center",
-        gap:"20px",
-        marginTop:"40px"
-    }}>
-        <h1>Global Identification</h1>
-        <p>Global identification of unique ideas starts with the recognition of truly innovative <br/>and one-of-a-kind concepts.</p>
-        <img src="../../../assests/global.png" alt='global' style={{width:"100%"}}/>
+    <div className="m-g">
+      <h1>Global Identification</h1>
+      <p>
+        Global identification of unique ideas starts with the recognition of
+        truly innovative <br />
+        and one-of-a-kind concepts.
+      </p>
+      <img
+        src="../../../assests/global.png"
+        alt="global"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Global
+export default Global;
