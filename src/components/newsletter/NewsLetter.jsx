fd734f0027a1857490ed3d18503e0a0014da2af9
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2023-10-10 00:43:09 +0530

    newsletter page updated

--- src/components/newsletter/NewsLetter.jsx
+++ src/components/newsletter/NewsLetter.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import "./NewsLetter.css";
 
 const NewsLetter = () => {
   return (
     <>
-      <img
-        src="../../../assests/newsletter.png"
-        alt="newsletter"
-        style={{
-          width: "100%",
-        }}
-      />
+      <div className="m-nl">
+        <div className="m-nl-l">
+          <h1>Subscribe NewsLetter</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
+            omnis, maiores quis voluptate sapiente quam neque optio inventore
+          </p>
+          <div className="m-nl-l-i">
+            <input type="email" placeholder="Enter email address" />
+            <button>Submit</button>
+          </div>
+        </div>
+
+        <div className="m-nl-r">
+          <img src="../../../assests/nlman.png" alt="nl-sideimage" />
+        </div>
+      </div>
     </>
   );
 };
